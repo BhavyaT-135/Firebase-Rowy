8f389cb525d9aaf10a3a362156217b6c7810791d
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-06-19 23:52:41 +0530

    ✨ Fetching data from firestore database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, 'users');
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    getUsers()
+  }, []);
+  
   return (
     <div className="App">
-      Hello World!!
+      <input placeholder='Name...' />
+      <input type='number' placeholder='Age...'/>
+      <button>Create User!</button>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h1>Name : {user.name}</h1>
+            <h1>Age : {user.age}</h1>
+          </div>
+        )
+      })}
     </div>
   );
 }
